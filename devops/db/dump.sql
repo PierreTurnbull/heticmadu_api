ee22951823de307e36f347d0dcca086dfc89d109
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-02-17 12:20:58 +0100

    Added db.

---
+++ devops/db/dump.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS heticmadu;
+CREATE DATABASE heticmadu;
+USE heticmadu;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DB,
+      logging: true,
+      entities: [
+        __dirname + '/../**/*.entity{.ts,.js}',
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
