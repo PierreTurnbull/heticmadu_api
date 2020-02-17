581ec36e744fa77c69eb6602de0b272e29fe4106
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-02-17 17:48:19 +0100

    Fixed mysql connection.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DB,
