cb74035cc0b594ab3c27a21ba7d14c2b643ef36c
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-02-18 14:25:32 +0100

    FUCK. THIS. SHIT.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-  Logger.log(`Application is running on port ${process.env.PORT}`, 'Bootstrap');
+  await app.listen(process.env.API_PORT);
+  Logger.log(`Application is running on port ${process.env.API_PORT}`, 'Bootstrap');
 }
 bootstrap();
