89f4dc5ae17bcfd5b8ccc04930b575504bc7e95c
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-02-19 14:55:11 +0100

    Added script for dump.

--- devops/dev/db/dump.sql
+++ devops/dev/db/dump.sql
@@ -1,17 +1,8 @@
--- noinspection SqlNoDataSourceInspectionForFile
-
--- noinspection SqlDialectInspectionForFile
-
--- MySQL dump 10.13  Distrib 8.0.18, for macos10.14 (x86_64)
---
--- Host: localhost    Database: heticmadu
--- ------------------------------------------------------
--- Server version	8.0.18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -19,13 +10,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `client`
---
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `heticmadu` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
+USE `heticmadu`;
 DROP TABLE IF EXISTS `client`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `client` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) DEFAULT NULL,
@@ -38,25 +28,16 @@ CREATE TABLE `client` (
   `type` varchar(32) DEFAULT NULL,
   `creationDate` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `client`
---
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `client_position`
---
-
 DROP TABLE IF EXISTS `client_position`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `client_position` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `longitude` varchar(128) DEFAULT NULL,
@@ -66,25 +47,16 @@ CREATE TABLE `client_position` (
   PRIMARY KEY (`id`),
   KEY `clientId` (`clientId`),
   CONSTRAINT `client_position_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `client_position`
---
 
 LOCK TABLES `client_position` WRITE;
 /*!40000 ALTER TABLE `client_position` DISABLE KEYS */;
 /*!40000 ALTER TABLE `client_position` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `point_of_interest`
---
-
 DROP TABLE IF EXISTS `point_of_interest`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `point_of_interest` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) DEFAULT NULL,
@@ -108,25 +80,16 @@ CREATE TABLE `point_of_interest` (
   PRIMARY KEY (`id`),
   KEY `clientId` (`clientId`),
   CONSTRAINT `point_of_interest_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `point_of_interest`
---
 
 LOCK TABLES `point_of_interest` WRITE;
 /*!40000 ALTER TABLE `point_of_interest` DISABLE KEYS */;
 /*!40000 ALTER TABLE `point_of_interest` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `point_of_interest_has_tags`
---
-
 DROP TABLE IF EXISTS `point_of_interest_has_tags`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `point_of_interest_has_tags` (
   `tagId` int(11) DEFAULT NULL,
   `poiId` int(11) DEFAULT NULL,
@@ -134,48 +97,30 @@ CREATE TABLE `point_of_interest_has_tags` (
   KEY `poiId` (`poiId`),
   CONSTRAINT `point_of_interest_has_tags_ibfk_1` FOREIGN KEY (`tagId`) REFERENCES `tag` (`id`),
   CONSTRAINT `point_of_interest_has_tags_ibfk_2` FOREIGN KEY (`poiId`) REFERENCES `point_of_interest` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `point_of_interest_has_tags`
---
 
 LOCK TABLES `point_of_interest_has_tags` WRITE;
 /*!40000 ALTER TABLE `point_of_interest_has_tags` DISABLE KEYS */;
 /*!40000 ALTER TABLE `point_of_interest_has_tags` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `tag`
---
-
 DROP TABLE IF EXISTS `tag`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tag` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tag`
---
 
 LOCK TABLES `tag` WRITE;
 /*!40000 ALTER TABLE `tag` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `firstName` varchar(128) DEFAULT NULL,
@@ -189,12 +134,8 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   KEY `clientId` (`clientId`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
@@ -210,4 +151,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-01-31 16:50:12
