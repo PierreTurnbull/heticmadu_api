fdca06b1e3ef299d8efd2f8f2474d6ccc2269bfa
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-02-20 16:58:17 +0100

    Updated database management: TypeORM now defines its structure.

---
+++ devops/dev/db/heticmadu.client-schema.sql
@@ -0,0 +1,16 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `numberOfEmployees` int(11) NOT NULL,
+  `perimeter` int(11) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `rse` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `creationDate` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
---
+++ devops/dev/db/heticmadu.point_of_interest-schema.sql
@@ -0,0 +1,30 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+
+CREATE TABLE `point_of_interest` (
+  `name` varchar(255) NOT NULL,
+  `longitude` int(11) NOT NULL,
+  `latitude` int(11) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `postalCode` int(11) NOT NULL,
+  `monday` varchar(255) NOT NULL,
+  `tuesday` varchar(255) NOT NULL,
+  `wednesday` varchar(255) NOT NULL,
+  `thursday` varchar(255) NOT NULL,
+  `friday` varchar(255) NOT NULL,
+  `saturday` varchar(255) NOT NULL,
+  `sunday` varchar(255) NOT NULL,
+  `schedule` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `averagePrice` varchar(255) NOT NULL,
+  `glutenFree` tinyint(4) NOT NULL,
+  `bio` varchar(255) NOT NULL,
+  `disabledAccess` tinyint(4) NOT NULL,
+  `greenScore` int(11) NOT NULL,
+  `phoneNumber` varchar(255) NOT NULL,
+  `picture` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
