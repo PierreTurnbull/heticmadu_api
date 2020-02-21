c608a3b0160ea78151a4326285d6b8afec27858a
billal.ouaali@hotmail.fr <billal.ouaali@hotmail.fr>
2020-02-21 17:17:49 +0100

    fix example

--- src/clients/examples/get-client-response.example.ts
+++ src/clients/examples/get-client-response.example.ts
@@ -1,4 +1,5 @@
 export const clientResponseExample = [{
+<<<<<<< HEAD
     id: 1,
     name: 'clientName',
     address: 'clientAddress',
@@ -24,10 +25,42 @@ export const clientResponseExample = [{
             longitude: '1.273828',
             latitude: '1.283783',
             perimeter: 10
+=======
+    clients: [
+        {
+            id: 1,
+            name: "clientName",
+            address: "clientAddress",
+            rse: "client@pattern.com",
+            numberOfEmployees: 5,
+            picture: "picture64",
+            perimeter: 10,
+            status: "Open",
+            type: "start-up",
+            creationDate: "2020-02-20T14:50:44+00:00",
+            clientPositions: [
+                {
+                    longitude: "1.273828",
+                    latitude: "1.273828",
+                    perimeter: 20
+                },
+                {
+                    longitude: "1.273828",
+                    latitude: "1.123423",
+                    perimeter: 15
+                },
+                {
+                    longitude: "1.273828",
+                    latitude: "1.283783",
+                    perimeter: 10
+                }
+            ]
+>>>>>>> 4f05918db8238435b0c7ea94665acf1a81065d09
         }
     ]
 },
     {
+<<<<<<< HEAD
         id: 2,
         name: 'clientName',
         address: 'clientAddress',
@@ -53,6 +86,37 @@ export const clientResponseExample = [{
                 longitude: '1.273828',
                 latitude: '1.283783',
                 perimeter: 10
+=======
+        clients: [
+            {
+                id: 2,
+                name: "clientName",
+                address: "clientAddress",
+                rse: "client@pattern.com",
+                numberOfEmployees: 5,
+                picture: "picture64",
+                perimeter: 10,
+                status: "Open",
+                type: "start-up",
+                creationDate: "2020-02-20T14:50:44+00:00",
+                clientPositions: [
+                    {
+                        longitude: "1.273828",
+                        latitude: "1.273828",
+                        perimeter: 20
+                    },
+                    {
+                        longitude: "1.273828",
+                        latitude: "1.123423",
+                        perimeter: 15
+                    },
+                    {
+                        longitude: "1.273828",
+                        latitude: "1.283783",
+                        perimeter: 10
+                    }
+                ]
+>>>>>>> 4f05918db8238435b0c7ea94665acf1a81065d09
             }
         ]
     }];
