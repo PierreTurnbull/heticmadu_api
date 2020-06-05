21c5211cd36e9943861bb6123446cffdb56485c7
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-06-05 14:25:08 +0200

    Added migration script.

---
+++ devops/prod/db/migrations/1591359650223-heticmadu-prod.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class heticmaduProd1591359650223 implements MigrationInterface {
+    name = 'heticmaduProd1591359650223'
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `point_of_interest` CHANGE `longitude` `longitude` float NOT NULL", undefined);
+        await queryRunner.query("ALTER TABLE `point_of_interest` CHANGE `latitude` `latitude` float NOT NULL", undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("ALTER TABLE `point_of_interest` CHANGE `latitude` `latitude` float(12) NOT NULL", undefined);
+        await queryRunner.query("ALTER TABLE `point_of_interest` CHANGE `longitude` `longitude` float(12) NOT NULL", undefined);
+    }
+
+}
